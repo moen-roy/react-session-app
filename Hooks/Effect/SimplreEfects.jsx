0987168d244d60b760bccd58469873bb9065ced1
moen <moenr49@gmail.com>
2025-07-17 10:25:04 +0300

    feat: implement form toggle functionality in SimpleEffect component

--- Hooks/Effect/SimplreEfects.jsx
+++ Hooks/Effect/SimplreEfects.jsx
@@ -9,13 +9,29 @@ function SimpleEffect() {
   // useEffect(function,[radius])-> initially when component is mounte and when radius
 
   const [n, setN] = useState(0);
+  const [showForm, setShowForm] = useState(false);
+
+  const getStatus = () => {
+    if (showForm === true) {
+      return "Hide Form";
+    }
+    return "Show Form";
+  };
 
   // Whenever state change occurs
   // It triggures a rerender of the component
   // if the useEffect does not have dependency it will rerender
+
+  //   useEffect(() => {
+  //     console.log("useEffect has run");
+  //   });
+
+  // Using use Effect with empty dependency
+  // Empty dependcy useEffect only once Intital render Only
+
   useEffect(() => {
     console.log("useEffect has run");
-  });
+  }, []);
 
   return (
     <div>
@@ -37,6 +53,33 @@ function SimpleEffect() {
           +
         </button>
       </div>
+      <button
+        onClick={() => {
+          if (showForm === true) {
+            setShowForm(false);
+          } else {
+            setShowForm(true);
+          }
+        }}
+      >
+        {getStatus()}
+      </button>
+      <MyForm showForm={showForm} />
+    </div>
+  );
+}
+
+function MyForm(props) {
+  const { showForm } = props;
+
+  if (showForm === false) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h3>This my Form</h3>
+      <input />
     </div>
   );
 }
