2e64df0bc338d6fe4407e713ebc002533ae97388
moen <moenr49@gmail.com>
2025-07-17 09:54:52 +0300

    refactor: enhance comments and structure in SimpleEffect component

--- Hooks/Effect/SimplreEfects.jsx
+++ Hooks/Effect/SimplreEfects.jsx
@@ -1,13 +1,42 @@
-import { use, useEffect } from "react"
+// ANGULAR-> LIFE CYCLE HOOK
+
+import { useEffect, useState } from "react";
 
 function SimpleEffect() {
-useEffect(() => {
+  // useEffect(function,[dependency])
+  // useEffect(function)-> without dependency
+  // useEffect(function,[])-> it will run only once
+  // useEffect(function,[radius])-> initially when component is mounte and when radius
+
+  const [n, setN] = useState(0);
+
+  // Whenever state change occurs
+  // It triggures a rerender of the component
+  // if the useEffect does not have dependency it will rerender
+  useEffect(() => {
     console.log("useEffect has run");
   });
 
   return (
     <div>
       <h1>Simple Effect</h1>
+      <div>
+        <button
+          onClick={() => {
+            setN(n - 1);
+          }}
+        >
+          -
+        </button>
+        <span>{n}</span>
+        <button
+          onClick={() => {
+            setN(n + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
     </div>
   );
 }
