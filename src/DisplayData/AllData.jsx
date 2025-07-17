3cd699d5b62d7f6d0f6890fae2b4d4af72e0d132
moen <moenr49@gmail.com>
2025-07-17 09:28:00 +0300

    feat: implement AllData component to display author information

---
+++ src/DisplayData/AllData.jsx
@@ -0,0 +1,13 @@
+function AllData(){
+        const author="Roy Moen";
+    let is_married = true;
+    let salary= 50000;
+
+    return (
+        <div>
+            <p>The aouthor of this is {author} </p>
+            <p> {author} is married? {is_married}. <br/> Earns a salary of {salary} </p>
+        
+        </div>
+    )
+    }
