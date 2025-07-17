ffb19471b2d8c3d4935d3bfa7d38d95346e81700
moen <moenr49@gmail.com>
2025-07-17 09:27:26 +0300

    feat: add Combined component with C1, C2, and C3 exports

---
+++ src/Combined.jsx
@@ -0,0 +1,18 @@
+function C1(){
+    return(
+        <h1>Combined exports c1</h1>
+    )
+}
+function C2(){
+    return(
+        <h2>Combined exports c2</h2>
+    )
+}
+function C3(){
+    return(
+        <h3>Combined exports c3</h3>        
+    )
+}
+export{C1};
+export default C2;
+export {C3}
