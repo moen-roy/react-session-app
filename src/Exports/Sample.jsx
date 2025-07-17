10d4cf878974c1736c99da33923decea65d266cd
moen <moenr49@gmail.com>
2025-07-17 09:28:14 +0300

    feat: add SampleComponent and TestComponent for demonstration purposes

---
+++ src/Exports/Sample.jsx
@@ -0,0 +1,21 @@
+
+function SampleComponent(){
+  return(
+    <>
+    <div>
+        <h1>Sample component</h1>
+        <h4>
+        This is a sample component to demonstrate the use of React and ESLint.
+      </h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa aperiam necessitatibus voluptatem natus eum voluptas libero placeat a, dolor officia animi quidem, rem sint doloremque eveniet! Asperiores iure quam laborum?</p>
+    </div>
+    <div>
+      <h5>Another Sample Component</h5>
+      <p>This component is here to show how components can be reused.</p>
+    </div>
+    </>
+    
+  )
+}
+
+export default SampleComponent;
---
+++ src/Exports/TestComponent.jsx
@@ -0,0 +1,19 @@
+function TestComponent(){
+    return (
+        <>
+        <div>
+            <h1>Test Component</h1>
+            <p> the sample function is now working</p>
+        </div>
+        <div>
+            <h6>Test Component</h6>
+            <p>This is a test component to demonstrate the use of React and ESLint.</p>
+        </div>
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, alias quos. Eveniet, optio nobis. Maiores, mollitia corrupti sed ullam natus optio a earum. Modi distinctio corporis voluptas veniam, tenetur nemo.</p>
+            
+        </div>   
+     </>
+    )
+}
+export default TestComponent;
