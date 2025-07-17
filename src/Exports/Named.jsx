71671a4ce33a5667e87116602839022d4f6eb582
moen <moenr49@gmail.com>
2025-07-17 09:27:42 +0300

    feat: add Named and Named2 components with named exports for demonstration

---
+++ src/Exports/Named.jsx
@@ -0,0 +1,28 @@
+function Content1(){
+    return(
+        <div>
+            <h1>Named Exports Example 1.1</h1>
+            <p>This component demonstrates the use of named exports in React.</p>
+         </div>
+    )
+    
+}
+function Content2(){
+     return(
+        <div>
+            <h1>Named Exports Example 2.2</h1>
+            <p>This component demonstrates the use of named exports in React.</p>
+         </div>
+    )
+    
+}
+function Content3(){
+     return(
+        <div>
+            <h1>Named Exports Example 3.3</h1>
+            <p>This component demonstrates the use of named exports in React.</p>
+         </div>
+    )
+    
+}
+export {Content1, Content2, Content3};
---
+++ src/Exports/Named2.jsx
@@ -0,0 +1,35 @@
+function DisplayCon1(){
+    return(
+        <div>
+            <h1>1 .</h1>
+            <p>my own</p>
+        </div>
+
+    )
+}
+function DisplayCon2(){
+    return(
+        <div>
+            <h1>2 .</h1>
+            <p>my own</p>
+        </div>
+
+    )
+}
+function DisplayCon3(){
+    return(
+        <>
+        <div>
+            <h1>3 .</h1>
+            <p>my own</p>
+        </div>
+        <div>
+            <h1>4 .</h1>
+            <p>my own</p>
+        </div>
+
+        </>
+        
+    )
+}
+export {DisplayCon1, DisplayCon2, DisplayCon3};
